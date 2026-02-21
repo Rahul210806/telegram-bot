c489d12d785619b436a5f72bd5b4fe5eab5b0827
Rahul Vishwakarma <muneshlohar100@gmail.com>
2026-02-21 16:13:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ async def get_file_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("/start", /start))
 app.add_handler(MessageHandler(filters.ALL, get_file_id))
 
 app.run_polling()
